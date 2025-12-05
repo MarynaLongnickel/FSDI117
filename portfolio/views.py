6633cb77968438528579139e4aa8affcc8828a94
Maryna Longnickel <maryna.longnickel@gmail.com>
2025-12-04 18:27:51 -0600

    fixed

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+
+def home_view(request):
+    return render(request, 'portfolio/home.html')
+
 def about_me_view(request):
     return render(request, 'portfolio/about_me.html')
